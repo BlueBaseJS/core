2648a09d50a35320766474e24565e38c41572cfc
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-08-18 12:02:24 +0500

    plugin class now entertains config

--- src/Plugin.js
+++ src/Plugin.js
@@ -1,5 +1,10 @@
 // base class of a plugin which is to be extended
 export default class Plugin {
+
+	constructor(config) {
+		this.config = config;
+	}
+
     // To initialize Plagin i.e To add all the callbacks against the specific plugin
 	initialize() {
 
