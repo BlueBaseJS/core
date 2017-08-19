a1b989f75c42b5ef43b24d12195b040b411296b9
Umair Farooq <umair.farooq@blueeast.com>
2017-08-19 16:02:35 +0500

    App registry test added

--- tests/unit/AppRegistry.test.js
+++ tests/unit/AppRegistry.test.js
@@ -1,33 +1,41 @@
-// import AppRegistry from '../../src/registries/AppRegistry';
-// const App = require('bluerain-app-hello-world');
+import React from 'react';
+import AppRegistry from '../../src/registries/AppRegistry';
+import App from '../../src/models/App';
 
-// describe('App registry tests', () => {
-// 	describe('register app', () => {
-// 		it('should throw error b/c name is undefined', () => {
-// 			expect(() => registerApp(undefined, App)).toThrow();
-// 		});
-// 		it('should throw error b/c name is null', () => {
-// 			expect(() => registerApp(null, App)).toThrow();
-// 		});
+const HelloApp = () => (
+  <div>
+    hello world
+  </div>
+  );
 
-// 		it('should have Apps', () => {
-// 			registerApp('hello', App);
-// 			expect(AppsTable.hasOwnProperty('hello')).to.equal(true);
-// 		});
-// 	});
-// 	describe('remove app', () => {
-// 		it('should throw error b/c name is undefined', () => {
-// 			expect(() => removeApp(undefined)).toThrow();
-// 		});
-// 		it('should throw error b/c name is null', () => {
-// 			expect(() => removeApp(null)).toThrow();
-// 		});
-// 		it('should throw error b/c app is not registered.', () => {
-// 			expect(() => removeApp('abc')).toThrow();
-// 		});
-// 		it('should have Apps', () => {
-// 			removeApp('hello');
-// 			expect(AppsTable.hasOwnProperty('hello')).to.equal(false);
-// 		});
-// 	});
-// });
+const app = new App({ name: 'Hello World', component: HelloApp });
+describe('App registry tests', () => {
+	describe('register app', () => {
+		it('should throw error b/c name is undefined', () => {
+			expect(() => AppRegistry.register(undefined)).toThrow();
+		});
+		it('should throw error b/c name is null', () => {
+			expect(() => AppRegistry.register(null)).toThrow();
+		});
+
+		it('should have Apps', () => {
+			AppRegistry.register( app);
+			expect(AppRegistry.AppsTable.hasOwnProperty('hello-world')).toEqual(true);
+		});
+	});
+	describe('remove app', () => {
+		it('should throw error b/c name is undefined', () => {
+			expect(() => AppRegistry.removeApp(undefined)).toThrow();
+		});
+		it('should throw error b/c name is null', () => {
+			expect(() => AppRegistry.removeApp(null)).toThrow();
+		});
+		it('should throw error b/c app is not registered.', () => {
+			expect(() => AppRegistry.removeApp('abc')).toThrow();
+		});
+		it('should have Apps', () => {
+			AppRegistry.removeApp('hello-world');
+			expect(AppRegistry.AppsTable.hasOwnProperty('hello')).toEqual(false);
+		});
+	});
+});
