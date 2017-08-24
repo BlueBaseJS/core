bd4d3c661265cda0374da026b36f7facbfa4584e
Umair Farooq <umair.farooq@blueeast.com>
2017-08-24 11:56:16 +0500

    Solved bugs

--- tests/integration/common/plugins/InternationalizationPlugin/index.js
+++ tests/integration/common/plugins/InternationalizationPlugin/index.js
@@ -2,27 +2,31 @@
  * Created by umair on 8/22/17.
  */
 import React from 'react';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import { Plugin, CallbackRegistry } from '../../../../../src/';
+import { IntlProvider, addLocaleData, FormattedMessage } from 'react-intl';
+import { Plugin, CallbackRegistry, ComponentRegistry } from '../../../../../src/';
 
-const messages = CallbackRegistry.run('bluerain.internationalization.lang', {});
-
-const withInternationalization = (App, locale) => props => (
-  <IntlProvider locale={locale} messages={messages[locale]} >
+const withInternationalization = (App, locale) => (props) => {
+	const messages = CallbackRegistry.run(
+    'bluerain.intl.messages',
+    {}
+  );
+	return (
+  <IntlProvider locale={locale} messages={messages[locale]}>
     <App {...props} />
   </IntlProvider>
-);
+	);
+};
 
 class InternationalizationPlugin extends Plugin {
 	static pluginName = 'InternationalizationPlugin';
 	static slug = 'Internationalization';
 	static initialize(config = {}) {
 		if (!config.locale) {
-			config.locale = 'ur';
+			config.locale = 'en';
 		}
 		const locale = config.locale;
 		const localeData = require(`react-intl/locale-data/${locale}`);
-
+		ComponentRegistry.register('FormattedMessage', FormattedMessage);
 		addLocaleData(localeData);
     // Add internationalization to main system app
 		CallbackRegistry.add(
