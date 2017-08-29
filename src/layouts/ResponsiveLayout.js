5e42669f32e25a6d8422fd1ec6b253e89c747c6f
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-08-29 21:56:09 +0500

    ResponsiveLayout now accepts string as prop

--- src/layouts/ResponsiveLayout.js
+++ src/layouts/ResponsiveLayout.js
@@ -3,6 +3,7 @@ import React from 'react';
 import RX from 'reactxp';
 import PropTypes from 'prop-types';
 
+import ComponentRegistry from '../registries/ComponentRegistry';
 import { withWindowInfo } from '../plugins/WindowInfoPlugin';
 
 class ResponsiveLayout extends RX.Component {
@@ -10,19 +11,27 @@ class ResponsiveLayout extends RX.Component {
 	render() {
 
 		const { window, default: def, xs, xm, md, lg, xl, ...props } = this.props; // eslint-disable-line no-unused-vars
-		const Component = (this.props[window.size]) ? this.props[window.size] : def;
+		let Component = (this.props[window.size]) ? this.props[window.size] : def;
+
+		if (typeof Component === 'string') {
+			Component = ComponentRegistry.get('Component');
+		}
+
+		if (!Component) {
+			throw new Error('Invalid Component.');
+		}
 
 		return (<Component {...props} />);
 	}
 }
 
 ResponsiveLayout.propTypes = {
-	default: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
-	xs: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
-	sm: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
-	md: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
-	lg: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
-	xl: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+	default: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.string]).isRequired,
+	xs: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.string]),
+	sm: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.string]),
+	md: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.string]),
+	lg: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.string]),
+	xl: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.string]),
 };
 
 export default withWindowInfo(ResponsiveLayout);
