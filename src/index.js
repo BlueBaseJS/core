37b20bb462d3a4b564591d735fa4ea9834e71b33
amnajunaid <amna.junaid@blueeast.com>
2017-08-30 18:40:13 +0500

    fixed some lint issues

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-export * from './boot';
 import RX from 'reactxp';
-// Models
-export App from './models/App';
-export Plugin from './models/Plugin';
 
 // Registries
 import AppRegistry from './registries/AppRegistry';
 import CallbackRegistry from './registries/FilterRegistry';
-export ComponentRegistry from './registries/ComponentRegistry';
-export ConfigRegistry from './registries/ConfigRegistry';
 import PluginRegistry from './registries/PluginRegistry';
 import EventRegistry from './registries/Events';
 
-//This will have all registries as objects
+export * from './boot';
+// Models
+export App from './models/App';
+export Plugin from './models/Plugin';
+
+
+export ComponentRegistry from './registries/ComponentRegistry';
+export ConfigRegistry from './registries/ConfigRegistry';
+
+// This will have all registries as objects
 export const BlueRain = {
-  apps:AppRegistry,
-  plugins: PluginRegistry,
-  filters: CallbackRegistry,
-  events: EventRegistry
+	apps:AppRegistry,
+	plugins: PluginRegistry,
+	filters: CallbackRegistry,
+	events: EventRegistry
 };
 
-//This will have ReactXPApi as objects
+// This will have ReactXPApi as objects
 export const ReactXP = {
-  location: RX.Location,
-  network: RX.Network,
-  platform: RX.Platform,
-  app: RX.App
+	location: RX.Location,
+	network: RX.Network,
+	platform: RX.Platform,
+	app: RX.App
 };
 
 // Router
