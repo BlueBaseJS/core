b42ee40eb571779f8799ec028c6a818ed277ebe8
haris_ramzan <haris.ramzan@blueeast.com>
2017-08-30 11:38:18 +0500

    Export ReactXP apis like App, Platform, Network, Location, etc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export * from './boot';
-
+import RX from 'reactxp';
 // Models
 export App from './models/App';
 export Plugin from './models/Plugin';
@@ -10,11 +10,23 @@ import CallbackRegistry from './registries/FilterRegistry';
 export ComponentRegistry from './registries/ComponentRegistry';
 export ConfigRegistry from './registries/ConfigRegistry';
 import PluginRegistry from './registries/PluginRegistry';
+import EventRegistry from './registries/Events';
 
+//This will have all registries as objects
 export const BlueRain = {
   apps:AppRegistry,
   plugins: PluginRegistry,
-  filters: CallbackRegistry
+  filters: CallbackRegistry,
+  events: EventRegistry
 };
+
+//This will have ReactXPApi as objects
+export const ReactXP = {
+  location: RX.Location,
+  network: RX.Network,
+  platform: RX.Platform,
+  app: RX.App
+};
+
 // Router
 export * from './router';
