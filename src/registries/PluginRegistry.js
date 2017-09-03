9a497b501568bafad4ed83c5eedee61a97a251cc
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-03 12:51:13 +0500

    fixed a relative path issue

--- src/registries/PluginRegistry.js
+++ src/registries/PluginRegistry.js
@@ -3,7 +3,7 @@
 import get from 'lodash.get';
 import kebabCase from 'lodash.kebabcase';
 
-import BR from '../../src';
+import BR from '../index';
 
 /**
  * All system plugins are stored in this registry
