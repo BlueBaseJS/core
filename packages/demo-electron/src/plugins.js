1dfaa9f62285a40b5d7057292bf08d8f51eb006f
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-06 15:07:35 +0500

    web & electron

---
+++ packages/demo-electron/src/plugins.js
@@ -0,0 +1,3 @@
+module.exports = [
+	// require('@blueeast/bluerain-plugin-boilerplate').default
+];
--- packages/web/src/plugins.js
+++
@@ -1,3 +0,0 @@
-module.exports = [
-	require('@blueeast/bluerain-plugin-boilerplate').default
-];
