0fff267dee7ac366216cae7b68d502ea2203e3a5
Umair Farooq <umair.farooq@blueeast.com>
2017-09-07 10:57:58 +0500

    Plugins unit tests

--- tests/unit/Plugins/InternationalizationPlugin.test.js
+++ tests/unit/Plugins/InternationalizationPlugin.test.js
@@ -1,3 +1,26 @@
 /**
  * Created by umair on 9/6/17.
  */
+import React from 'react';
+import RX from 'reactxp';
+import { shallow } from 'enzyme';
+import BR from '../../../src';
+import InternationalizationPlugin
+  from '../../../src/plugins/InternationalizationPlugin';
+describe('Internationalization Plugin tests', () => {
+	it('', () => {
+		class HelloWorld extends BR.App {
+			static appName = 'Hello World';
+			render() {
+				return <div>Hello world</div>;
+			}
+    }
+		const element = BR.boot({
+			apps: [HelloWorld],
+			plugins: [InternationalizationPlugin],
+			renderApp: false
+		});
+		const wrapper = shallow(<element />);
+		expect(wrapper.find('IntlProvider')).toBeTruthy();
+	});
+});
