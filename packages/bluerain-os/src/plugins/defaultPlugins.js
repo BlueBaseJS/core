7f93c0909e1b231ee9120b681a0a7e558b2b0e54
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-08 21:15:08 +0500

    added ReduxPlugin as a default system plugin in bluerain. This helps us avoid any breaking changes

--- packages/bluerain-os/src/plugins/defaultPlugins.js
+++ packages/bluerain-os/src/plugins/defaultPlugins.js
@@ -1,6 +1,8 @@
 // This file registers builtin plugins that ship with BlueRain OS
+import ReduxPlugin from '@blueeast/bluerain-plugin-redux';
 import WindowInfoPlugin from './WindowInfoPlugin';
 
 export default [
+	ReduxPlugin,
 	WindowInfoPlugin
 ];
