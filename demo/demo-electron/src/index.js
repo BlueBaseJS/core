a04e151ad09607ca708500a3e8cfc1c3bf3893e8
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-14 18:35:49 +0500

    added dependencies to electron demo

--- demo/demo-electron/src/index.js
+++ demo/demo-electron/src/index.js
@@ -1,10 +1,12 @@
 /*
 * This file demonstrates using BlueRain Client on web
 */
-const plugins = require('./plugins');
 const BR = require('@blueeast/bluerain-os').default;
+const config = require('./config');
+const plugins = require('./plugins');
 
 const bootOptions = {
+	config,
 	plugins
 };
 
