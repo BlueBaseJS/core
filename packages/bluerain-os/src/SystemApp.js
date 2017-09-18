dc7d780737ca53e72def038fff1412e1ec16aac7
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-18 10:56:07 +0500

    New way to hook into system routes

--- packages/bluerain-os/src/SystemApp.js
+++ packages/bluerain-os/src/SystemApp.js
@@ -4,37 +4,47 @@ import BR from './index';
 
 import { parseJsonSchema } from './utils/JsonSchemaToReact';
 
-const Routes = () => {
-
+export const SystemRoutes = () => {
 	const appRoutes = BR.Apps.getComponentSchema();
+
+	// Default Routes
+	let systemRoutes = [
+		{
+			component: 'Route',
+			props: {
+				path: '/',
+				exact: true,
+				component: BR.Components.get('IndexPage')
+			}
+		},
+		...appRoutes
+	];
+
+	// Dynamically add new routes
+	systemRoutes = BR.Filters.run('bluerain.system.routes', systemRoutes);
+
+	// Add 404 at the end
+	systemRoutes.push({
+		component: 'Route',
+		props: {
+			component: BR.Components.get('NotFoundPage')
+		}
+	});
+
+	return systemRoutes;
+};
+
+export default () => {
 
 	const routes = {
 		component: 'SystemLayout',
 		children: [
 			{
 				component: 'Switch',
-				children: [
-					{
-						component: 'Route',
-						props: {
-							path: '/',
-							exact: true,
-							component: BR.Components.get('IndexPage')
-						}
-					},
-					...appRoutes,
-					{
-						component: 'Route',
-						props: {
-							component: BR.Components.get('NotFoundPage')
-						}
-					}
-				]
+				children: SystemRoutes()
 			}
 		]
 	};
 
-	return parseJsonSchema(BR.Filters.run('bluerain.system.routes', routes));
+	return parseJsonSchema(BR.Filters.run('bluerain.system.app.schema', routes));
 };
-
-export default Routes;
