203d814102f0866485be3d8cc53e5ee6b1d3b91c
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-19 22:26:15 +0500

    user locale is automatically detected in web apps

--- packages/bluerain-plugin-intl/src/defaultConfigs.js
+++ packages/bluerain-plugin-intl/src/defaultConfigs.js
@@ -1,3 +1,5 @@
+import { language } from './detect';
+
 /**
  * This is the default configuration set that is used at boot time.
  *
@@ -6,7 +8,7 @@
  * @property {Array}	localeData				An array of localData files.
  */
 const IntlPluginConfigs = {
-	locale: 'en',
+	locale: language || 'en',
 	localeData: [],
 	selectable: {
 		en: 'English'
