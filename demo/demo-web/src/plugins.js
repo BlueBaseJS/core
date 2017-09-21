8187ba975609e55f1b4d4bb65d6141a3ac35432f
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-09-21 18:51:57 +0500

    updated deps

--- demo/demo-web/src/plugins.js
+++ demo/demo-web/src/plugins.js
@@ -3,5 +3,5 @@ module.exports = [
 	require('@blueeast/bluerain-plugin-intl').default,
 	require('@blueeast/bluerain-plugin-material-ui').default,
 	require('@blueeast/bluerain-plugin-redux-devtools').default,
-	// require('@blueeast/bluerain-plugin-why-did-you-update').default,
+	require('@blueeast/bluerain-plugin-why-did-you-update').default,
 ];
