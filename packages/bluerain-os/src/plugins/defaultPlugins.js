c40bb92ee473063ff79d8bad8a401dc8146b4771
Umair Farooq <umair.farooq@blueeast.com>
2017-10-03 14:25:30 +0500

    for travis eslint issue check

--- packages/bluerain-os/src/plugins/defaultPlugins.js
+++ packages/bluerain-os/src/plugins/defaultPlugins.js
@@ -1,6 +1,6 @@
 // This file registers builtin plugins that ship with BlueRain OS
 import ReactRouterPlugin from '@blueeast/bluerain-plugin-react-router';
-import ReduxPlugin from '@blueeast/bluerain-plugin-redux';
+import ReduxPlugin from '@blueeast/bluerain-plugin-redux';// eslint-disable-line
 import WindowInfoPlugin from './WindowInfoPlugin';
 
 export default [
