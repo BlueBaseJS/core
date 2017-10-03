4841e2fe2b780be1bb890e49c5957abb1a39d647
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-10-03 13:11:20 +0300

    new filter registry

--- packages/bluerain-os/src/registries/FilterRegistry.js
+++ packages/bluerain-os/src/registries/FilterRegistry.js
@@ -1,37 +1,69 @@
 /* @flow */
 
-import reject from 'lodash.reject';
+import isFunction from 'lodash.isfunction';
+import isNil from 'lodash.isnil';
+
+import { List } from 'immutable';
+
+import MapRegistry from './MapRegistry';
+
+type FilterItem = {
+	name: string,
+	filter: Function
+};
 
 /**
  * All system filters are stored in this registry
  * @property {Object} FiltersTable Storage table of all filters
  */
-class FilterRegistry {
+class FilterRegistry extends MapRegistry {
 
-	FiltersTable: {} = {};
+	data: Map<string, List<FilterItem>>;
+
+	constructor() {
+		super('FilterRegistry');
+	}
 
 	/**
 	 * Add a filter function to a hook
 	 * @param {String} hook - The name of the hook
 	 * @param {Function} filter - The filter function
 	 */
-	add(hook: string, filter: Function) {
-		if (hook === undefined || hook === null) {
+	set(hook: string, name: string | Function, filter: Function, index: number) {
+		if (isNil(hook)) {
 			throw new Error(`hook cannot be ${hook}`);
 		}
-		if (!filter.name) {
-			console.warn(
-				`// Warning! You are adding an unnamed filter to ${hook}.
-			Please use the function foo () {} syntax.`
-			);
+
+		// If a plugin is using an old system of sending named functions
+		if (isFunction(name)) {
+			filter = name;
+			name = filter.name;
 		}
 
-		// if filter array doesn't exist yet, initialize it
-		if (typeof this.FiltersTable[hook] === 'undefined') {
-			this.FiltersTable[hook] = [];
+		if (isNil(name)) {
+			throw new Error(`You are adding an unnamed filter to ${hook}.`);
 		}
 
-		this.FiltersTable[hook].push(filter);
+		if (isNil(filter)) {
+			throw new Error(`You have to provide a filter function to ${hook}.`);
+		}
+
+		let list = this.data.get(hook);
+
+		if (!list) {
+			list = List();
+		}
+
+		// Check if this filter already exists
+		if (list.findIndex(item => item.name === name) > -1) {
+			throw new Error(`Filter ${name} already exists in ${hook} hook.`);
+		}
+
+		const item = { name, filter };
+
+		list = (isNil(index)) ? list.push(item) : list.insert(index, item);
+
+		this.data.set(hook, list);
 	}
 
 	/**
@@ -39,30 +71,28 @@ class FilterRegistry {
 	 * @param {string} hookName - The name of the hook
 	 * @param {string} filterName - The name of the function to remove
 	 */
-	remove(hookName: string, filterName: string) {
-		if (hookName === undefined || hookName === null) {
-			throw new Error(`hook cannot be ${hookName}`);
+	remove(hook: string, name: string) {
+		if (isNil(hook)) {
+			throw new Error(`hook cannot be ${hook}`);
 		}
 
-		if (filterName === undefined || filterName === null) {
-			throw new Error(`filter of ${hookName} cannot be ${filterName}`);
+		if (isNil(name)) {
+			throw new Error(`filter name cannot be ${name}`);
 		}
 
-		if (!Object.prototype.hasOwnProperty.call(this.FiltersTable, hookName)) {
-			throw new Error(`${hookName}  is not added. First add hook to remove it.`);
+		if (!this.data.has(hook)) {
+			throw new Error(`${hook} filter is not added. First add filter to remove it.`);
 		}
 
-		const noOffilters = this.FiltersTable[hookName].length;
-		this.FiltersTable[hookName] = reject(
-			this.FiltersTable[hookName],
-			filter => filter.name === filterName
-		);
-		if (this.FiltersTable[hookName].length === noOffilters) {
-			throw new Error(
-				`${hookName} has no filter named ${filterName}.
-			First add filter in ${hookName} to remove it`
-			);
+		let list = this.data.get(hook);
+		const index = list.findIndex(item => item.name === name);
+
+		if (index === -1) {
+			throw new Error(`${name} filter is not added in ${hook} hook. First add filter to remove it.`);
 		}
+
+		list = list.delete(index);
+		this.data.set(hook, list);
 	}
 
 	/**
@@ -73,52 +103,30 @@ class FilterRegistry {
 	 * @param {Any} args - Other arguments will be passed to each successive iteration
 	 * @returns {Object} Returns the item after it's been through all the filters for this hook
 	 */
-	run(hook: string, item: {}) {
-		// the first argument is the name of the hook or an array of functions
-		// const hook = arguments[0];
-		// the second argument is the item on which to iterate
-		// const item = arguments[1];
-		// successive arguments are passed to each iteration
-		const sliceNumber = 2;
-		const args = Array.prototype.slice.call(arguments).slice(sliceNumber); // eslint-disable-line prefer-rest-params
-		if (hook === undefined || hook === null) {
+	run(hook, item) {
+		if (isNil(hook)) {
 			throw new Error(`hook cannot be ${hook}`);
 		}
+		const sliceNumber = 2;
+		const args = Array.prototype.slice.call(arguments).slice(sliceNumber); // eslint-disable-line prefer-rest-params
 
-		const filters = Array.isArray(hook) ? hook : this.FiltersTable[hook];
+		const filters = this.data.get(hook);
 
-		if (typeof filters !== 'undefined' && !!filters.length) {
-			// if the hook exists, and contains filters to run
-			return filters.reduce((accumulator, filter) => {
-				const newArguments = [accumulator].concat(args);
+		if (isNil(filters) || filters.size === 0) {
+			return item;
+		}
 
-				try {
-					const result = filter.apply({}, newArguments);
+		return filters.reduce((accumulator, item) => {
+			const newArguments = [accumulator].concat(args);
+			const result = item.filter.apply({}, newArguments);
 
-					if (typeof result === 'undefined') {
-						// if result of current iteration is undefined, don't pass it on
-						// console.log(
-						//   `// Warning: Sync filter [${filter.name}] in hook
-						// [${hook}] didn't return a result!`
-						// );
-						return accumulator;
-					}
-					return result;
-				} catch (error) {
-					// console.log(
-					//   `// error at filter [${filter.name}] in hook [${hook}]`
-					// );
-					// console.log(error);
-					throw error;
-					// if (error.break || error.data && error.data.break) {
-
-					// }
-					// pass the unchanged accumulator to the next iteration of the loop
-					// return accumulator;
-				}
-			}, item);
-		} // else, just return the item unchanged
-		return item;
+			if (typeof result === 'undefined') {
+				// if result of current iteration is undefined, don't pass it on
+				console.warn(`Warning: Sync filter [${item.name}] in hook [${hook}] didn't return a result!`);
+				return accumulator;
+			}
+			return result;
+		}, item);
 	}
 }
 
