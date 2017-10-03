6709922dae6fd7844284f0079429cae5f42fb0df
Umair Farooq <umair.farooq@blueeast.com>
2017-10-03 15:22:21 +0500

    travis lint issue

--- demo/demo-electron/src/plugins.js
+++ demo/demo-electron/src/plugins.js
@@ -1,5 +1,5 @@
 module.exports = [
-	require('@blueeast/bluerain-plugin-apollo').default, // eslint disable-line
+	require('@blueeast/bluerain-plugin-apollo').default, // eslint-disable-line
 	// require('@blueeast/bluerain-plugin-intl').default,
-	require('@blueeast/bluerain-plugin-redux-devtools').default// eslint disable-line
+	require('@blueeast/bluerain-plugin-redux-devtools').default// eslint-disable-line
 ];
