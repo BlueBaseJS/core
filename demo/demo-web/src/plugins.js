f0b224a710ea2c65fee676c8bd81627edc8a4c5b
amnajunaid <amna.junaid@blueeast.com>
2017-10-04 10:29:02 +0500

    Launcher plugin added

--- demo/demo-web/src/plugins.js
+++ demo/demo-web/src/plugins.js
@@ -5,5 +5,6 @@ module.exports = [
 	require('@blueeast/bluerain-plugin-persist-redux').default,
 	require('@blueeast/bluerain-plugin-redux-devtools').default,
 	require('@blueeast/bluerain-plugin-redux').default,
-	require('@blueeast/bluerain-plugin-taskbar').default
+	require('@blueeast/bluerain-plugin-taskbar').default,
+	require('@blueeast/bluerain-plugin-launcher').default
 ];
