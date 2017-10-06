5da42d664b5b3b5a8b83d8295a2a2b3cd5d139a4
amnajunaid <amna.junaid@blueeast.com>
2017-10-06 17:09:05 +0500

    Fixed lint errors in launcher plugin

--- packages/bluerain-plugin-launcher/src/common/Launcher/LauncherContainer.js
+++ packages/bluerain-plugin-launcher/src/common/Launcher/LauncherContainer.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import { withBlueRain } from '@blueeast/bluerain-os';
-import FileFolder from 'material-ui/svg-icons/file/folder';
 import BulbIcon from '../icons/Bulb.component';
 import Launcher from './LauncherComponent';
 const forEach = require('lodash.foreach');
@@ -43,4 +42,8 @@ class Container extends React.Component {
 	}
 
 }
+
+Container.propTypes = {
+  bluerain: PropTypes.object // eslint-disable-line
+};
 export default withBlueRain(Container);
