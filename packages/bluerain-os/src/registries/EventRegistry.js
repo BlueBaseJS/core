3a421b6eb4800828ccd94b186a1b0db02b6294dd
Umair Farooq <umair.farooq@blueeast.com>
2017-10-24 15:05:55 +0500

    fixed docs

--- packages/bluerain-os/src/registries/EventRegistry.js
+++ packages/bluerain-os/src/registries/EventRegistry.js
@@ -1,5 +1,5 @@
 /**
- * All system filters are stored in this registry.It is using [event emitter](https://github.com/primus/eventemitter3) to emit and add events.
+ * All system events are stored in this registry.It is using [event emitter](https://github.com/primus/eventemitter3) to emit and add events.
  *
  */
 import EventEmitter from 'eventemitter3';
