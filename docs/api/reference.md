cef1cc6ca9ccf9fe79cf9b8a9864c58e118483e2
Umair Farooq <umair.farooq@blueeast.com>
2017-10-24 14:45:20 +0500

    docs updated

--- docs/api/reference.md
+++ docs/api/reference.md
@@ -397,7 +397,7 @@ Register many configs at once
 
 ## EventEmitter
 
-Created by umair on 8/28/17.
+All system filters are stored in this registry.It is using [event emitter](https://github.com/primus/eventemitter3) to emit and add events.
 
 ## FilterRegistry
 
--- packages/bluerain-os/src/registries/EventRegistry.js
+++ packages/bluerain-os/src/registries/EventRegistry.js
@@ -1,5 +1,6 @@
 /**
- * Created by umair on 8/28/17.
+ * All system filters are stored in this registry.It is using [event emitter](https://github.com/primus/eventemitter3) to emit and add events.
+ *
  */
 import EventEmitter from 'eventemitter3';
 export default EventEmitter;
