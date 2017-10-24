8015c91ea98e9648618afcb2d51c98a03c273e5f
Umair Farooq <umair.farooq@blueeast.com>
2017-10-24 15:23:45 +0500

    typescript initial project setup

---
+++ packages/bluerain-os-typescript/test/bluerain-os-typescript.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/bluerain-os-typescript"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
