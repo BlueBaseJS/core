57b330e7b9dba9fe7927f702721c20f467bfabae
Umair Farooq <umair.farooq@blueeast.com>
2017-12-11 17:59:01 +0500

    added jsdocs

--- packages/bluerain-os/src/registries/HooksRegistry.js
+++ packages/bluerain-os/src/registries/HooksRegistry.js
@@ -3,6 +3,10 @@ import isNil from 'lodash.isnil';
 import FilterRegistry from './FilterRegistry';
 import EventRegistry from './EventRegistry';
 
+/**
+ * All system hooks are stored in this registry
+ *
+ */
 export default class HookRegistry {
 	filters:FilterRegistry;
 	events:EventRegistry;
@@ -11,6 +15,11 @@ export default class HookRegistry {
 		this.filters = new FilterRegistry();
 		this.events = new EventRegistry();
 	}
+    /**
+	 * Add a filter function to a hook.
+	 * @param {String} hook - The name of the hook
+	 * @param {Function} filter - The filter function
+	 */
 	add(hook:string, filter: Function) {
 		if (isNil(hook)) {
 			throw new Error(`You are adding an invalid hook:${hook}.`);
@@ -22,16 +31,22 @@ export default class HookRegistry {
 		this.filters.set(hook, filter);
 		this.events.on(hook, filter);
 	}
+    /**
+	 * Successively run all of a hook's functions on an item
+	 * @param {String} hook - First argument: the name of the hook
+	 * @param {'async' |'sync' | 'both'} mode - Second argument: mode in which hook will run. If not given mode will be sync
+	 * @param {Any} args - Other arguments will be passed to each successive iteration
+	 * @returns {Object} Returns the item after it's been through all the filters for this hook
+	 */
 	run(hook:string, mode: 'async' |'sync' | 'both'= 'sync', ...args) {
 		if (isNil(hook)) {
 			throw new Error(`Hook cannot be ${hook}. Please provide valid hook while running it.`);
 		}
 		if (mode === 'both') {
-
-			this.filters.run(hook, ...args);
 			this.events.emit(hook, ...args);
+			return this.filters.run(hook, ...args);
 		} else if (mode === 'sync') {
-			this.filters.run(hook, ...args);
+			return this.filters.run(hook, ...args);
 		} else if (mode === 'async') {
 			this.events.emit(hook, ...args);
 		}
