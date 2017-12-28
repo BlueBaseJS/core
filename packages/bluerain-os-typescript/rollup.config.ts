c3c66e25ee43b7ad3b3f7952fef7ca96265a3bb9
Umair Farooq <umair.farooq@blueeast.com>
2017-12-28 12:53:58 +0500

    added sizes rollup plugin

--- packages/bluerain-os-typescript/rollup.config.ts
+++ packages/bluerain-os-typescript/rollup.config.ts
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
+import sizes from 'rollup-plugin-sizes'
 import camelCase from 'lodash.camelcase'
 
 const pkg = require('./package.json')
@@ -15,7 +16,7 @@ export default {
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external:  Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
+	external:  Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
   watch: {
     include: 'compiled/**',
   },
@@ -26,7 +27,7 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
+		sizes(),
     // Resolve source maps to the original source
     sourceMaps(),
   ],
--- packages/bluerain-os-typescript/src/Provider.ts
+++ packages/bluerain-os-typescript/src/Provider.ts
@@ -1,13 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { getContext, withContext } from 'recompose';
 import BR from './index';
 
-const BlueRainProvider = withContext({ bluerain: PropTypes.object }, props => ({
+const BlueRainProvider = withContext({ bluerain: {} }, props => ({
 	bluerain: BR
 }))(props => React.Children.only(props.children));
 
 const withBlueRain = Component =>
-	getContext({ bluerain: PropTypes.object })(Component);
+	getContext({ bluerain: {} })(Component);
 
 export { BlueRainProvider, withBlueRain };
