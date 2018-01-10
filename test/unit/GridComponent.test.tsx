e5415a8634725020f66cc1b0a0cd555f0a9371b2
adnan1994 <adnan.naeem@blueeast.com>
2018-01-10 12:35:59 +0500

    fix(Fixed pull request): Fixed pull request
    Fixed pull request

--- test/unit/GridComponent.test.tsx
+++ test/unit/GridComponent.test.tsx
@@ -6,22 +6,31 @@ import {getComponentWidth,getComponentOffset,isHidden} from '../../src/Component
 import BR, { App } from '../../src';
 import Adapter from 'enzyme-adapter-react-16';
 import {mount, shallow,  render, configure} from 'enzyme';
+import Index from '../../src/pages/IndexPage';
+import NotFound from '../../src/pages/NotFoundPage';
+import Page from '../../src/pages/Page';
 
 configure({ adapter: new Adapter() });
+
+
+const View=()=>{
+
+  return 'View';
+
+  };
+
+BR.boot({renderApp:false});
+BR.Components.register('View',View);
 
 describe('GridComponents tests', () => {
 
     document.body.innerHTML = '<div class="app-container"></div>';
 
 it('Component column', () => {
-  BR.boot({renderApp:false});
   const wrapper = mount(<Col/>);
   expect(wrapper.find('Col'));
 });
-
-
 it('Component Row', () => {
-
   const wrapper = mount(<Row />);
   expect(wrapper.find('Row'));
 });
@@ -29,10 +38,10 @@ it('Component Row', () => {
 it('Component Helpers', () => {
 
  getComponentWidth(768,'sm');
-getComponentWidth(768,'md');
-  getComponentWidth(768,'lg');
-  getComponentWidth(768,'xl');
-  getComponentOffset(89,'lg');
+ getComponentWidth(768,'md');
+ getComponentWidth(768,'lg');
+ getComponentWidth(768,'xl');
+ getComponentOffset(89,'lg');
 
   getComponentOffset(768,'sm');
   getComponentOffset(768,'md');
