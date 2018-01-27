786c70f505491b191e2ad21b8832bde43e09821b
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-01-27 11:15:14 +0500

    fix(Provider): Fixed and issue with Provider that was crashing boot

--- src/Provider.ts
+++ src/Provider.ts
@@ -1,11 +1,12 @@
 import React from 'react';
 import { getContext, withContext } from 'recompose';
+import PropTypes from 'prop-types';
 import BR from './index';
 
-const BlueRainProvider = withContext({}, props => ({
+const BlueRainProvider = withContext({ bluerain: PropTypes.object }, props => ({
 	bluerain: BR
 }))(props => React.Children.only(props.children));
 
-const withBlueRain = Component => getContext({})(Component);
+const withBlueRain = Component => getContext({ bluerain: PropTypes.object })(Component);
 
 export { BlueRainProvider, withBlueRain };
