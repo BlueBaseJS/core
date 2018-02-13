f628c9adabc0b8fa1e3359c15b071d7a97b51156
Babar Javaid <babar.javaid@blueeast.com>
2018-02-13 20:23:51 +0500

    fix(gh-pages): Resolve lint issues and modify user

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,31 +1,31 @@
-const { cd, exec, echo, touch } = require("shelljs")
-const { readFileSync } = require("fs")
-const url = require("url")
+const { cd, exec, echo, touch } = require('shelljs');
+const { readFileSync } = require('fs');
+const url = require('url');
 
-let repoUrl
-let pkg = JSON.parse(readFileSync("package.json") as any)
-if (typeof pkg.repository === "object") {
-  if (!pkg.repository.hasOwnProperty("url")) {
-    throw new Error("URL does not exist in repository section")
-  }
-  repoUrl = pkg.repository.url
+let repoUrl;
+let pkg = JSON.parse(readFileSync('package.json') as any);
+if (typeof pkg.repository === 'object') {
+	if (!pkg.repository.hasOwnProperty('url')) {
+		throw new Error('URL does not exist in repository section');
+	}
+	repoUrl = pkg.repository.url;
 } else {
-  repoUrl = pkg.repository
+	repoUrl = pkg.repository;
 }
 
-let parsedUrl = url.parse(repoUrl)
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
+let parsedUrl = url.parse(repoUrl);
+let repository = (parsedUrl.host || '') + (parsedUrl.path || '');
+let ghToken = process.env.GH_TOKEN;
 
-echo("Deploying docs!!!")
-cd("dist/docs")
-touch(".nojekyll")
-exec("git init")
-exec("git add .")
-exec('git config user.name "Umair Farooq"')
-exec('git config user.email "umair.farooq@blueeast.com"')
-exec('git commit -m "docs(docs): update gh-pages"')
+echo('Deploying docs!!!');
+cd('dist/docs');
+touch('.nojekyll');
+exec('git init');
+exec('git add .');
+exec('git config user.name "Babar Javaid"');
+exec('git config user.email "javaidbabar@gmail.com"');
+exec('git commit -m "docs(docs): update gh-pages"');
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
-)
-echo("Docs deployed!!")
+);
+echo('Docs deployed!!');
