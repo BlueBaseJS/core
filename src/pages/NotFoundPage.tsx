59cd9db9f0be94d7b4ae761e6e032e5dda696dd6
adnan1naeem <adnan.naeem@blueeast.com>
2018-07-03 17:13:00 +0500

    fixed issue of height in Notfound OOage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -12,9 +12,11 @@ const FOFIcon = () => <Icon title="404" />;
  *
  * @returns {React.Component} The layout react component
  */
-const NotFoundPage = ({ bluerain: BR }: { bluerain: BlueRain }) => (
-	<BR.Components.Wallpaper>
-		<BR.Components.Page>
+const NotFoundPage = ({ bluerain: BR }: { bluerain: BlueRain }) => {
+	console.log('saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+	return(
+	<BR.Components.Wallpaper style={{ height:100+'vh' }}>
+		<BR.Components.Page >
 			<BR.Components.CenterLayout style={{ padding: 20 }}>
 				<BR.Components.ComponentState
 					title="Page not found!"
@@ -23,7 +25,7 @@ const NotFoundPage = ({ bluerain: BR }: { bluerain: BlueRain }) => (
 				/>
 			</BR.Components.CenterLayout>
 		</BR.Components.Page>
-	</BR.Components.Wallpaper>
-);
+	</BR.Components.Wallpaper>);
+};
 
 export default withBlueRain(NotFoundPage);
