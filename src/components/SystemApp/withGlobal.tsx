0361900e5abd829956bdd412729a7bac48105a02
amnajunaid <amna.junaid@blueeast.com>
2018-09-27 10:51:41 +0500

    fix(SystemApp.tsx): System App wrapped with global object.

--- src/components/SystemApp/withGlobal.tsx
+++ src/components/SystemApp/withGlobal.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const withGlobal = (WrappedCompopnent: React.ComponentType) => {
+const withGlobal = (WrappedCompopnent: React.ComponentType): React.ComponentType => {
 	const _global: any = global;
 	if (typeof _global.self === 'undefined') {
 		_global.self = global;
 	}
-	return <WrappedCompopnent />;
+	return WrappedCompopnent;
 };
 
 export default withGlobal;
