c3b83d19444a527565f020d2ce05ae9753beb00d
omarnaeem <omar.naeem@blueeast.com>
2018-11-10 15:36:26 +0500

    fix(🐛  Component): HoverObserver timerIds type fixed

--- src/components/HoverObserver/HoverObserver.tsx
+++ src/components/HoverObserver/HoverObserver.tsx
@@ -1,26 +1,26 @@
 import React, { SyntheticEvent } from 'react';
-import { Noop } from '../Noop';
 import { MaybeRenderPropChildren } from '../../utils';
+import { Noop } from '../Noop';
 
 interface MouseEventObject {
-  e: SyntheticEvent,
-  setIsHovering: () => void,
-  unsetIsHovering: () => void,
+	e: SyntheticEvent,
+	setIsHovering: () => void,
+	unsetIsHovering: () => void,
 }
 
 export interface HoverObserverProps {
-  hoverDelayInMs: number,
-  hoverOffDelayInMs: number,
-  onHoverChanged: (state: HoverObserverState) => void,
-  onMouseEnter: (obj: MouseEventObject) => void,
-  onMouseLeave: (obj: MouseEventObject) => void,
-  onMouseOver: (obj: MouseEventObject) => void,
-  onMouseOut: (obj: MouseEventObject) => void,
-  children?: MaybeRenderPropChildren<HoverObserverState>
+	hoverDelayInMs: number,
+	hoverOffDelayInMs: number,
+	onHoverChanged: (state: HoverObserverState) => void,
+	onMouseEnter: (obj: MouseEventObject) => void,
+	onMouseLeave: (obj: MouseEventObject) => void,
+	onMouseOver: (obj: MouseEventObject) => void,
+	onMouseOut: (obj: MouseEventObject) => void,
+	children?: MaybeRenderPropChildren<HoverObserverState>
 }
 
 export interface HoverObserverState {
-  readonly isHovering: boolean;
+	readonly isHovering: boolean;
 }
 
 /**
@@ -30,130 +30,130 @@ export interface HoverObserverState {
  */
 export class HoverObserver extends React.PureComponent<HoverObserverProps, HoverObserverState> {
 
-  public static defaultProps: Partial<HoverObserverProps> = {
-    hoverDelayInMs: 0,
-    hoverOffDelayInMs: 0,
-    onHoverChanged: Noop,
-    onMouseEnter: ({ setIsHovering }) => setIsHovering(),
-    onMouseLeave: ({ unsetIsHovering }) => unsetIsHovering(),
-    onMouseOver: Noop,
-    onMouseOut: Noop,
-  };
+	public static defaultProps: Partial<HoverObserverProps> = {
+		hoverDelayInMs: 0,
+		hoverOffDelayInMs: 0,
+		onHoverChanged: Noop,
+		onMouseEnter: ({ setIsHovering }) => setIsHovering(),
+		onMouseLeave: ({ unsetIsHovering }) => unsetIsHovering(),
+		onMouseOut: Noop,
+		onMouseOver: Noop,
+	};
 
-  private timerIds: NodeJS.Timeout[] = [];
+	private timerIds: number[] = [];
 
-  constructor(props: HoverObserverProps) {
-    super(props);
+	constructor(props: HoverObserverProps) {
+		super(props);
 
-    this.state = {
-        isHovering: false
-    };
+		this.state = {
+			isHovering: false
+		};
 
-    this.onMouseEnter = this.onMouseEnter.bind(this);
-    this.onMouseLeave = this.onMouseLeave.bind(this);
-    this.onMouseOver = this.onMouseOver.bind(this);
-    this.onMouseOut = this.onMouseOut.bind(this);
-    this.setIsHovering = this.setIsHovering.bind(this);
-    this.unsetIsHovering = this.unsetIsHovering.bind(this);
-    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+		this.onMouseEnter = this.onMouseEnter.bind(this);
+		this.onMouseLeave = this.onMouseLeave.bind(this);
+		this.onMouseOver = this.onMouseOver.bind(this);
+		this.onMouseOut = this.onMouseOut.bind(this);
+		this.setIsHovering = this.setIsHovering.bind(this);
+		this.unsetIsHovering = this.unsetIsHovering.bind(this);
+		this.componentWillUnmount = this.componentWillUnmount.bind(this);
 
-    this.timerIds = [];
-  }
+		this.timerIds = [];
+	}
 
-  onMouseEnter(e: SyntheticEvent) {
-    this.props.onMouseEnter({
-      e,
-      setIsHovering: this.setIsHovering,
-      unsetIsHovering: this.unsetIsHovering
-    });
-  }
+	onMouseEnter(e: SyntheticEvent) {
+		this.props.onMouseEnter({
+			e,
+			setIsHovering: this.setIsHovering,
+			unsetIsHovering: this.unsetIsHovering
+		});
+	}
 
-  onMouseLeave(e: SyntheticEvent) {
-    this.props.onMouseLeave({
-      e,
-      setIsHovering: this.setIsHovering,
-      unsetIsHovering: this.unsetIsHovering
-    });
-  }
+	onMouseLeave(e: SyntheticEvent) {
+		this.props.onMouseLeave({
+			e,
+			setIsHovering: this.setIsHovering,
+			unsetIsHovering: this.unsetIsHovering
+		});
+	}
 
-  onMouseOver(e: SyntheticEvent) {
-    this.props.onMouseOver({
-      e,
-      setIsHovering: this.setIsHovering,
-      unsetIsHovering: this.unsetIsHovering
-    });
-  }
+	onMouseOver(e: SyntheticEvent) {
+		this.props.onMouseOver({
+			e,
+			setIsHovering: this.setIsHovering,
+			unsetIsHovering: this.unsetIsHovering
+		});
+	}
 
-  onMouseOut(e: SyntheticEvent) {
-    this.props.onMouseOut({
-      e,
-      setIsHovering: this.setIsHovering,
-      unsetIsHovering: this.unsetIsHovering
-    });
-  }
+	onMouseOut(e: SyntheticEvent) {
+		this.props.onMouseOut({
+			e,
+			setIsHovering: this.setIsHovering,
+			unsetIsHovering: this.unsetIsHovering
+		});
+	}
 
-  componentWillUnmount() {
-    this.clearTimers();
-  }
+	componentWillUnmount() {
+		this.clearTimers();
+	}
 
-  setIsHovering() {
-    this.clearTimers();
+	setIsHovering() {
+		this.clearTimers();
 
-    const hoverScheduleId = setTimeout(() => {
-      const newState = { isHovering: true };
-      this.setState(newState, () => {
-        this.props.onHoverChanged(newState);
-      });
-    }, this.props.hoverDelayInMs);
+		const hoverScheduleId = setTimeout(() => {
+			const newState = { isHovering: true };
+			this.setState(newState, () => {
+				this.props.onHoverChanged(newState);
+			});
+		}, this.props.hoverDelayInMs);
 
-    this.timerIds.push(hoverScheduleId);
-  }
+		this.timerIds.push(hoverScheduleId);
+	}
 
-  unsetIsHovering() {
-    this.clearTimers();
+	unsetIsHovering() {
+		this.clearTimers();
 
-    const hoverOffScheduleId = setTimeout(() => {
-      const newState = { isHovering: false };
-      this.setState(newState, () => {
-          this.props.onHoverChanged(newState);
-      });
-    }, this.props.hoverOffDelayInMs);
+		const hoverOffScheduleId = setTimeout(() => {
+			const newState = { isHovering: false };
+			this.setState(newState, () => {
+				this.props.onHoverChanged(newState);
+			});
+		}, this.props.hoverOffDelayInMs);
 
-    this.timerIds.push(hoverOffScheduleId);
-  }
+		this.timerIds.push(hoverOffScheduleId);
+	}
 
-  clearTimers() {
-    const ids = this.timerIds;
-    while (ids.length) {
-      const id = ids.pop();
+	clearTimers() {
+		const ids = this.timerIds;
+		while (ids.length) {
+			const id = ids.pop();
 
-      if (id) {
-        clearTimeout(id);
-      }
-    }
-  }
+			if (id) {
+				clearTimeout(id);
+			}
+		}
+	}
 
-  renderChildrenWithProps(children: HoverObserverProps['children'], props: HoverObserverState) {
-    if (typeof children === 'function') {
-      return (children as any)(props);
-    }
+	renderChildrenWithProps(children: HoverObserverProps['children'], props: HoverObserverState) {
+		if (typeof children === 'function') {
+			return (children as any)(props);
+		}
 
-    return children;
-  }
+		return children;
+	}
 
 
-  render() {
-    const { children } = this.props;
+	render() {
+		const { children } = this.props;
 
-    return (
-      <div { ...{
-        onMouseEnter: this.onMouseEnter,
-        onMouseLeave: this.onMouseLeave,
-        onMouseOver: this.onMouseOver,
-        onMouseOut: this.onMouseOut
-      }}>
-        { this.renderChildrenWithProps(children, this.state) }
-      </div>
-    );
-  }
+		return (
+			<div {...{
+				onMouseEnter: this.onMouseEnter,
+				onMouseLeave: this.onMouseLeave,
+				onMouseOut: this.onMouseOut,
+				onMouseOver: this.onMouseOver,
+			}}>
+				{this.renderChildrenWithProps(children, this.state)}
+			</div>
+		);
+	}
 }
