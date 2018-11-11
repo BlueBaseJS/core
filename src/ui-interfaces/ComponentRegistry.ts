7005530925b5d81bb45cc2a6afa09dfbc4a02131
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-11-11 20:12:41 +0500

    feat(🎁 ComponentRegistry): Added missing component typings

--- src/ui-interfaces/ComponentRegistry.ts
+++ src/ui-interfaces/ComponentRegistry.ts
@@ -2,10 +2,13 @@
 import {
 	BlueBaseHookProps,
 	ComponentStateProps,
+	DataObserverProps,
 	DynamicIconProps,
+	ErrorObserverProps,
 	JsonSchemaProps,
 	LoadingStateProps,
 	PluginIconProps,
+	StatefulComponentProps,
 	SystemAppProps,
 	SystemContentProps,
 	WaitObserverProps,
@@ -13,21 +16,27 @@ import {
 import { ButtonProps as ButtonProperites } from './components/Button';
 import { ComponentRegistry } from '../registries';
 import React from 'react';
+import { ViewProperties } from 'react-native';
 
 export interface ComponentRegistryWithUIInterfaces extends ComponentRegistry {
 
 	// System Components
 	BlueBaseHook: React.ComponentType<BlueBaseHookProps>,
 	ComponentState: React.ComponentType<ComponentStateProps>,
+	DataObserver: React.ComponentType<DataObserverProps>,
 	DynamicIcon: React.ComponentType<DynamicIconProps>,
 	EmptyState: React.ComponentType,
+	ErrorObserver: React.ComponentType<ErrorObserverProps>,
 	ErrorState: React.ComponentType,
 	JsonSchema: React.ComponentType<JsonSchemaProps>,
 	LoadingState: React.ComponentType<LoadingStateProps>,
 	Noop: React.ComponentType,
 	PluginIcon: React.ComponentType<PluginIconProps>,
+	StatefulComponent: React.ComponentType<StatefulComponentProps>,
 	SystemApp: React.ComponentType<SystemAppProps>,
 	SystemContent: React.ComponentType<SystemContentProps>,
+	SystemFooter: React.ComponentType<ViewProperties>,
+	SystemHeader: React.ComponentType<ViewProperties>,
 	WaitObserver: React.ComponentType<WaitObserverProps>,
 
 	// UI
