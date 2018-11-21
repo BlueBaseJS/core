4d6d8952f67dfbefc1f7d392577f9194e3f15f6a
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-11-21 23:07:29 +0500

    wip: New Registry

---
+++ src/registries/NewRegistry/Examples/ComponentRegistry.ts
@@ -0,0 +1,16 @@
+// import { Registry } from '../Registry';
+
+// export interface ComponentRegistryItem {
+// 	value: React.ComponentType<any>;
+
+// 	hocs: any[]; // TODO: maybe each HOC should also be a BlueBaseModule?
+// 	source?: any;
+
+// 	isAsync: boolean;
+// 	preload: boolean;
+// }
+
+
+// export class ExampleComponentRegistry extends Registry<ComponentRegistryItem> {
+
+// }
