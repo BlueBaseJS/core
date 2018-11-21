5c5de4f4b90559e45fae612e5b8f40e4ab527c21
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-11-21 14:54:06 +0500

    test: Recreated snapshots, renamed test folders

---
+++ src/components/ComponentState/__tests__/ComponentState.test.tsx
@@ -0,0 +1,64 @@
+// import * as Component from '../..';
+import * as Native from '../../../native';
+import { BlueBase } from '../../../BlueBase';
+import { ComponentState } from '../ComponentState';
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+beforeEach(() => {
+	jest.resetModules();
+});
+
+const BB = new BlueBase();
+BB.Components.register('View', Native.View);
+BB.Components.register('Text', Native.Text);
+BB.Components.register('Image', Native.Image);
+const mockContext = jest.fn();
+
+jest.mock('../../../Context', () => ({
+	BlueBaseConsumer: ({ children }: { children: any }) => children(BB)
+}));
+
+describe('ComponentState', () => {
+	beforeEach(() => {
+		mockContext.mockReset();
+	});
+
+	test(`Snapshot ComponentState component`, () => {
+		const component = TestRenderer.create(
+			<ComponentState />
+		);
+		const tree = component.toTree();
+		expect(tree).toMatchSnapshot();
+	});
+
+	test(`Snapshot ComponentState component with description`, () => {
+		const component = TestRenderer.create(
+			<ComponentState description={'This is just for test'} />
+		);
+		const tree = component.toTree();
+		expect(tree).toMatchSnapshot();
+	});
+
+	test(`Snapshot ComponentState component with description and image`, () => {
+		const component = TestRenderer.create(
+			<ComponentState description={'This is just for test'}
+				image={<Native.Image
+					style={{ width: 50, height: 50 }}
+					source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+				/>} />
+		);
+		const tree = component.toTree();
+		expect(tree).toMatchSnapshot();
+	});
+
+	test(`Snapshot ComponentState component with description and imageSource`, () => {
+		const component = TestRenderer.create(
+			<ComponentState description={'This is just for test'}
+				imageSource={'hello'} />
+		);
+		const tree = component.toTree();
+		expect(tree ? tree.props.imageSource : tree).toEqual('hello');
+	});
+
+});
