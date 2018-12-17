f4e102fc32afb6a16651d1c14b0014890f9968e9
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-12-17 14:21:13 +0500

    test(🍱 JsonSchema): Added tests, 100% coverage

--- src/components/JsonSchema/__tests__/JsonSchema.test.tsx
+++ src/components/JsonSchema/__tests__/JsonSchema.test.tsx
@@ -8,44 +8,99 @@ beforeEach(() => {
 });
 
 describe('JsonSchema', () => {
-	const JsonSchemaWithProvider = (props: any) => (
-		<BlueBaseApp>
-			<JsonSchema {...props}/>
-		</BlueBaseApp>
-	);
 
-	test(`Snapshot ComponentState component`, () => {
+	test(`should render a text component with red color`, (done) => {
 		const component = TestRenderer.create(
-			<JsonSchemaWithProvider schema={{
-				component: 'Text',
-				props: {
-					style: {
-						color: 'red'
-					}
-				},
-				text: 'This componenet is generated through JsonSchema Component',
-			}}/>
+			<BlueBaseApp>
+				<JsonSchema schema={{
+					component: 'Text',
+					props: {
+						style: {
+							color: 'red'
+						}
+					},
+					text: 'This component is generated through JsonSchema Component',
+				}} />
+			</BlueBaseApp>
 		);
-		const tree = component.toJSON();
+		let tree = component.toJSON();
+		expect((tree as any).children.join()).toBe('Loading');
 		expect(tree).toMatchSnapshot();
-	});
 
-	test(`Snapshot ComponentState component after complete rendering`, (done) => {
-		const component = TestRenderer.create(
-			<JsonSchemaWithProvider schema={{
-				component: 'Text',
-				props: {
-					style: {
-						color: 'red'
-					}
-				},
-				text: 'This componenet is generated through JsonSchema Component',
-			}}/>
-		);
+
 		setTimeout(() => {
-			const tree = component.toJSON();
+			tree = component.toJSON();
+			expect((tree as any).children[0].props.style[1]).toMatchObject({
+				color: 'red'
+			});
+			expect((tree as any).children[0].children.join()).toBe('This component is generated through JsonSchema Component');
 			expect(tree).toMatchSnapshot();
 			done();
 		});
 	});
+
+	test(`should render a text component with red color that is processed by hooks`, (done) => {
+
+		const hook = (schema: any, args: { style: any }) => ({
+			...schema,
+			props: {
+				...schema.props,
+				style: {
+					...schema.props.style,
+					...args.style,
+				}
+			},
+			text: 'Content changed by hook!',
+		});
+
+		const component = TestRenderer.create(
+			<BlueBaseApp hooks={{ 'content-hook': hook }}>
+				<JsonSchema
+					hook="content-hook"
+					args={{ style: { color: 'blue' } }}
+					schema={{
+						component: 'Text',
+						props: {
+							style: {
+								color: 'red'
+							}
+						},
+						text: 'This component is generated through JsonSchema Component',
+					}
+				} />
+			</BlueBaseApp>
+		);
+
+		setTimeout(() => {
+			const tree = component.toJSON();
+			expect((tree as any).children[0].props.style[1]).toMatchObject({
+				color: 'blue'
+			});
+			expect((tree as any).children[0].children.join()).toBe('Content changed by hook!');
+			expect(tree).toMatchSnapshot();
+			done();
+		});
+	});
+
+	test(`should render a null for unknown component`, (done) => {
+
+		const component = TestRenderer.create(
+			<BlueBaseApp>
+				<JsonSchema schema={{
+					component: 'Foo',
+					text: 'This component is generated through JsonSchema Component',
+				}} />
+			</BlueBaseApp>
+		);
+
+
+		setTimeout(() => {
+			const tree = component.toJSON();
+			expect(tree).toMatchSnapshot();
+			expect((tree as any).children.join())
+				.toBe('Could not parse React JSON Schema. Reason: Could not resolve component.');
+			done();
+		});
+	});
+
 });
