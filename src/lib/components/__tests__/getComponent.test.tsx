6da9d42ba2144c5efe2a7ba12034e3605e440e23
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-12-17 15:06:26 +0500

    test(getComponent): Added tests, 100% coverage

---
+++ src/lib/components/__tests__/getComponent.test.tsx
@@ -0,0 +1,51 @@
+import { Button, Text } from '../index';
+import { BlueBaseApp } from '../../../components';
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+beforeEach(() => {
+	jest.resetModules();
+});
+
+describe('getComponent', () => {
+
+	test(`should render a text component`, (done) => {
+		const component = TestRenderer.create(
+			<BlueBaseApp>
+				<Text>A Text component</Text>
+			</BlueBaseApp>
+		);
+
+		let tree = component.toJSON();
+		expect((tree as any).children.join()).toBe('Loading');
+		expect(tree).toMatchSnapshot();
+
+		setTimeout(() => {
+			tree = component.toJSON();
+			expect((tree as any).children[0].type).toBe('Text');
+			expect((tree as any).children[0].children.join()).toBe('A Text component');
+			expect(tree).toMatchSnapshot();
+			done();
+		});
+	});
+
+	test(`should render a button component`, (done) => {
+		const component = TestRenderer.create(
+			<BlueBaseApp>
+				<Button>A Button component</Button>
+			</BlueBaseApp>
+		);
+
+		let tree = component.toJSON();
+		expect((tree as any).children.join()).toBe('Loading');
+		expect(tree).toMatchSnapshot();
+
+		setTimeout(() => {
+			tree = component.toJSON();
+			expect((tree as any).children[0].type).toBe('View');
+			expect(tree).toMatchSnapshot();
+			done();
+		});
+	});
+
+});
