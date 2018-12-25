c83c4fa3de6db308559866a3ca798422ebd7652c
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-12-25 23:21:17 +0500

    docs(🚨 ErrorState): Improved docs

--- src/components/ErrorState/ErrorState.tsx
+++ src/components/ErrorState/ErrorState.tsx
@@ -13,11 +13,16 @@ export interface ErrorStateProps {
 }
 
 /**
- * 🚨 ErrorState
+ * # 🚨 ErrorState
  *
  * Display an error message. Used by UIs when an exception is caught, and an error message
  * needs to be displayed. If in development mode, the actual error is displayed, otherwise
  * displays a generic message in production mode.
+ *
+ * ## Usage
+ * ```jsx
+ * <ErrorState retry={retryCallback} error={Error('Bang!')} />
+ * ```
  */
 export class ErrorState extends React.PureComponent<ErrorStateProps> {
 
