e74914075c0a35ff3c1550b62561de0727ad2671
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-03-09 14:06:44 +0500

    test(📕 storybook): Partial update to V5

--- bluebase/storybook/configs/webpack.config.js
+++ bluebase/storybook/configs/webpack.config.js
@@ -1,35 +1,36 @@
-// const { storybookWebpackConfigs } = require('@blueeast/bluerain-cli-essentials');
-const path = require('path');
+const { storybookWebpackConfigs } = require('@bluebase/cli-essentials');
+// const path = require('path');
 // const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 
-module.exports = (baseConfig, env, config) => {
+module.exports = ({ config }) => {
 	
-	// config = storybookWebpackConfigs(baseConfig, env, config);
+	config = storybookWebpackConfigs({ config });
 
-	config.module.rules.push({
+	// config.module.rules.push({
 		
-		test: /\.(ts|tsx)$/,
-    include: [
-			path.resolve(__dirname, '../../../src'),
-			path.resolve(__dirname, '../../../bluebase')
-		],
-		use: [
-			{
-				loader: require.resolve('ts-loader'),
-			},
-			// require.resolve('react-docgen-typescript-loader'),
-		],
-	});
-	config.resolve.extensions.push(".ts", ".tsx");
+	// 	test: /\.(ts|tsx)$/,
+  //   include: [
+	// 		path.resolve(__dirname, '../../../bluebase'),
+	// 		path.resolve(__dirname, '../../../src/index'),
+	// 	],
+	// 	use: [
+	// 		{
+	// 			loader: require.resolve('ts-loader'),
+	// 		},
+	// 		// require.resolve('react-docgen-typescript-loader'),
+	// 	],
+	// });
+	// config.resolve.extensions.push(".ts", ".tsx");
 
-	// Alias react-native
-	config.resolve.alias['react-native$'] = require.resolve('react-native-web');
-	config.resolve.alias['react-art$'] = require.resolve('react-art');
-
-	// Alias legacy projects
-	config.resolve.alias['@bluebase/core$'] = path.resolve(__dirname, '../../../src');
-	config.resolve.alias['@blueeast/bluerain$'] = path.resolve(__dirname, '../../../src');
-	config.resolve.alias['@blueeast/bluerain-os$'] = path.resolve(__dirname, '../../../src');
+	// // Alias react-native
+	// config.resolve.alias['react-native$'] = require.resolve('react-native-web');
+	// config.resolve.alias['react-art$'] = require.resolve('react-art');
+	
+	// // Alias legacy projects
+	// config.resolve.alias['@bluebase/core$'] = path.resolve(__dirname, '../../../src');
+	// config.resolve.alias['@blueeast/bluerain$'] = path.resolve(__dirname, '../../../src');
+	// config.resolve.alias['@blueeast/bluerain-os$'] = path.resolve(__dirname, '../../../src');
+	console.log(config)
 
 	return config;
 };
