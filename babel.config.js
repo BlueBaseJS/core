4b5c4501d55e68106a3f6932c486e021045c56a9
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-05-16 20:37:48 +0500

    build(babel): fixed code styles

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "presets": [
-    "@bluebase/code-standards/babel.config"
+  'presets': [
+    '@bluebase/code-standards/babel.config'
   ],
-  "env": {
-    "test": {
-      "plugins": [
-        ["istanbul", {
-          "exclude": [
-            "**/*.test.{ts,tsx,js,jsx}",
-            "tests/*.{ts,tsx,js,jsx}"
+  'env': {
+    'test': {
+      'plugins': [
+        ['istanbul', {
+          'exclude': [
+            '**/*.test.{ts,tsx,js,jsx}',
+            'tests/*.{ts,tsx,js,jsx}'
           ]
         }]
       ]
