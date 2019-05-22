09a96e009e78ad1384be14f4b79e52929bfcc649
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-05-23 04:34:16 +0500

    test(🖼 BlueBaseImage): Added tests, improved coverage

--- src/components/BlueBaseImage/BlueBaseImage.tsx
+++ src/components/BlueBaseImage/BlueBaseImage.tsx
@@ -1,9 +1,9 @@
 import { BlueBaseImageProps, ImageProps } from '@bluebase/components';
+
 import { BlueBase } from '../../BlueBase';
 import { BlueBaseContext } from '../../Context';
 import { Image } from '../../getComponent';
 import React from 'react';
-
 
 // tslint:disable: jsdoc-format
 /**
@@ -19,21 +19,19 @@ import React from 'react';
 ```
  */
 export class BlueBaseImage extends React.PureComponent<BlueBaseImageProps> {
-
 	static contextType = BlueBaseContext;
 
 	public static defaultProps = {
-		args: {}
+		args: {},
 	};
 
 	render() {
-
 		const { resolve, source: _source, ...rest } = this.props;
 		const BB: BlueBase = this.context;
 
 		const source = resolveImageSource(this.props, BB);
 
-		if (source === null) {
+		if (!source) {
 			return null;
 		}
 
@@ -42,35 +40,29 @@ export class BlueBaseImage extends React.PureComponent<BlueBaseImageProps> {
 }
 
 export const resolveImageSource = (props: BlueBaseImageProps, BB: BlueBase) => {
-
 	const { resolve, source: _source } = props;
 
 	let Asset;
 
 	if (typeof _source === 'string') {
 		Asset = BB.Assets.resolve(_source);
-	}
-	else if (Array.isArray(_source)) {
-
+	} else if (Array.isArray(_source)) {
 		const keys = (_source as string[]).filter(s => typeof s === 'string');
 
 		if (keys.length > 0) {
 			Asset = BB.Assets.resolve(...keys);
 		}
-	}
-	else if (resolve) {
+	} else if (resolve) {
 		BB.Logger.warn('resolve prop in BlueBaseImage is deprecated. Use "source" instead');
 
-		Asset = Array.isArray(resolve)
-		? BB.Assets.resolve(...resolve)
-		: BB.Assets.get(resolve);
+		Asset = Array.isArray(resolve) ? BB.Assets.resolve(...resolve) : BB.Assets.get(resolve);
 	}
 
 	if ((typeof _source === 'string' || Array.isArray(_source) || resolve) && !Asset) {
 		return null;
 	}
 
-	const source = Asset ? Asset.value : _source as ImageProps['source'];
+	const source = Asset ? Asset.value : (_source as ImageProps['source']);
 
 	return source;
 };
--- src/components/BlueBaseImage/__tests__/BlueBaseImage.test.tsx
+++ src/components/BlueBaseImage/__tests__/BlueBaseImage.test.tsx
@@ -5,9 +5,7 @@ import { mount } from 'enzyme';
 import { waitForElement } from 'enzyme-async-helpers';
 
 describe('BlueBaseImage', () => {
-
-	test(`should render a BlueBlueImage`, async() => {
-
+	test(`should render a BlueBlueImage`, async () => {
 		const wrapper = mount(
 			<BlueBaseApp assets={{ Logo: './Logo.jpg' }}>
 				<BlueBaseImage resolve="Logo" source={{ uri: 'ignore.png' }} />
@@ -16,14 +14,17 @@ describe('BlueBaseImage', () => {
 
 		await waitForElement(wrapper as any, BlueBaseImage);
 
-		expect(wrapper.find('Image').first().prop('source')).toMatchObject({
-			'uri': './Logo.jpg',
+		expect(
+			wrapper
+				.find('Image')
+				.first()
+				.prop('source')
+		).toMatchObject({
+			uri: './Logo.jpg',
 		});
-
 	});
 
-	test(`should render a BlueBlueImage`, async() => {
-
+	test(`should render a BlueBlueImage`, async () => {
 		const wrapper = mount(
 			<BlueBaseApp assets={{ Logo: './Logo.jpg' }}>
 				<BlueBaseImage source="Logo" />
@@ -32,15 +33,17 @@ describe('BlueBaseImage', () => {
 
 		await waitForElement(wrapper as any, BlueBaseImage);
 
-		expect(wrapper.find('Image').first().prop('source')).toMatchObject({
-			'uri': './Logo.jpg',
+		expect(
+			wrapper
+				.find('Image')
+				.first()
+				.prop('source')
+		).toMatchObject({
+			uri: './Logo.jpg',
 		});
-
 	});
 
-
 	test(`should render a backup BlueBlueImage`, async () => {
-
 		const wrapper = mount(
 			<BlueBaseApp assets={{ Logo: './Logo.jpg' }}>
 				<BlueBaseImage resolve={['Icon', 'Logo']} source={{ uri: 'ignore.png' }} />
@@ -49,14 +52,17 @@ describe('BlueBaseImage', () => {
 
 		await waitForElement(wrapper as any, BlueBaseImage);
 
-		expect(wrapper.find('Image').first().prop('source')).toMatchObject({
-			'uri': './Logo.jpg',
+		expect(
+			wrapper
+				.find('Image')
+				.first()
+				.prop('source')
+		).toMatchObject({
+			uri: './Logo.jpg',
 		});
 	});
 
-
 	test(`should render an image from source if none is found`, async () => {
-
 		const wrapper = mount(
 			<BlueBaseApp assets={{ None: require('./Logo.jpg') }}>
 				<BlueBaseImage resolve={['Icon', 'Logo']} source={{ uri: 'ignore.png' }} />
@@ -65,14 +71,17 @@ describe('BlueBaseImage', () => {
 
 		await waitForElement(wrapper as any, BlueBaseImage);
 
-		expect(wrapper.find('Image').first().prop('source')).toMatchObject({
-			'uri': 'test-file-stub',
+		expect(
+			wrapper
+				.find('Image')
+				.first()
+				.prop('source')
+		).toMatchObject({
+			uri: 'test-file-stub',
 		});
 	});
 
-
 	test(`should render a backup BlueBlueImage from source prop`, async () => {
-
 		const wrapper = mount(
 			<BlueBaseApp assets={{ Logo: './Logo.jpg' }}>
 				<BlueBaseImage source={['Icon', 'Logo']} />
@@ -81,9 +90,37 @@ describe('BlueBaseImage', () => {
 
 		await waitForElement(wrapper as any, BlueBaseImage);
 
-		expect(wrapper.find('Image').first().prop('source')).toMatchObject({
-			'uri': './Logo.jpg',
+		expect(
+			wrapper
+				.find('Image')
+				.first()
+				.prop('source')
+		).toMatchObject({
+			uri: './Logo.jpg',
 		});
 	});
 
+	test(`should render null when there is any empty array in source`, async () => {
+		const wrapper = mount(
+			<BlueBaseApp assets={{ Logo: './Logo.jpg' }}>
+				<BlueBaseImage source={[]} />
+			</BlueBaseApp>
+		);
+
+		await waitForElement(wrapper as any, BlueBaseImage);
+
+		expect(wrapper.find('Image').exists()).toBe(false);
+	});
+
+	test(`should render null if there is no source`, async () => {
+		const wrapper = mount(
+			<BlueBaseApp assets={{ Logo: './Logo.jpg' }}>
+				<BlueBaseImage />
+			</BlueBaseApp>
+		);
+
+		await waitForElement(wrapper as any, BlueBaseImage);
+
+		expect(wrapper.find('Image').exists()).toBe(false);
+	});
 });
