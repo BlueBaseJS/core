a7f96ad9f7e95fec31b5143f3d544baa6bebeaf7
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-12-21 10:13:16 +0500

    chore(storybook-native): Working as expected

--- bluebase/expo/bluebase.ts
+++ bluebase/expo/bluebase.ts
@@ -1,24 +1,12 @@
-import { BootOptions } from '@bluebase/core';
+import { BootOptions } from '../../src';
 import commonBootOptions from '../common/bluebase';
 import deepmerge from 'deepmerge';
-
-// const assetsPath = `../../assets/expo`;
 
 /**
  * Add your platform specific configs here.
  * We keep all the universal (cross platform) configs in
  * the common folder, and extend them here.
  */
-const bootOptions: Partial<BootOptions> = {
-
-	// config: {
-
-	// 	wallpaper: {
-	// 		backgroundColor: 'white',
-	// 		resizeMode: 'cover',
-	// 		source: require(`${assetsPath}/wallpaper.jpg`),
-	// 	},
-	// }
-};
+const bootOptions: Partial<BootOptions> = {};
 
 export default deepmerge(commonBootOptions, bootOptions);
