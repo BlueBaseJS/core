7162d2f7041b0548a6a61c7f7a26b678cea750b0
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-07-23 16:25:17 +0500

    feat: Use expo-font to load fonts

--- src/registries/helpers/loadFont.native.ts
+++
@@ -1,9 +0,0 @@
-import { FontValue } from '../FontRegistry';
-
-declare const global: any;
-
-export const loadAllFonts = async (fonts: { [name: string]: FontValue }) => {
-	if (global.__expo && global.__expo.Font && global.__expo.Font.loadAsync) {
-		await global.__expo.Font.loadAsync(fonts);
-	}
-};
--- src/registries/helpers/loadFont.ts
+++ src/registries/helpers/loadFont.ts
@@ -1,29 +1,7 @@
+import * as Font from 'expo-font';
+
 import { FontValue } from '../FontRegistry';
 
-declare const document: any;
-
-export const loadFont = (fontName: string, font: string) => {
-	const iconFontStyles = `@font-face {
-		src: url(${font});
-		font-family: ${fontName};
-	}`;
-
-	// Create stylesheet
-	const style: any = document.createElement('style');
-	style.type = 'text/css';
-
-	if (style.styleSheet) {
-		style.styleSheet.cssText = iconFontStyles;
-	} else {
-		style.appendChild(document.createTextNode(iconFontStyles));
-	}
-
-	// Inject stylesheet
-	document.head.appendChild(style);
-};
-
 export const loadAllFonts = async (fonts: { [name: string]: FontValue }) => {
-	Object.keys(fonts).forEach((fontName: string) => {
-		loadFont(fontName, fonts[fontName] as string);
-	});
+	Font.loadAsync(fonts);
 };
